315e8d325bd7522fb6b9f50782c7aefc935cb8f6
xxrl <837951112@qq.com>
2021-12-27 00:59:08 +0800

    Add README and support set file path from args

---
+++ README.md
@@ -0,0 +1,16 @@
+# RustNESEmulator
+
+Hello, this is a NES Emulator base on [SimpleNES](https://github.com/amhndu/SimpleNES) implemented by Rust.
+
+
+## Build
+
+We use [SFML](https://docs.rs/sfml/latest/sfml/) binding on c++ to show game window, so SFML 2.5 and CSFML 2.5 must be installed.
+
+Note due to the performance issue, you should run in release mode to get enough frame rate to fresh screen.
+
+## TODO
+
+1. Audio support
+2. More Rom type support
+3. Pause/Resume/Save
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 mod lib;
 use lib::*;
+
+use clap::Parser;
+
 #[macro_use]
 extern crate ini;
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+  #[clap(short, long, default_value = "assets/test.nes")]
+  rom_path: String,
+
+  #[clap(short, long, default_value = "assets/keybindings.ini")]
+  key_binding_path: String,
+}
 
 fn main() {
   match crate::logger::init() {
     Err(_) => return,
     Ok(_) => {}
   };
+  let args = Args::parse();
   let mut emulator = lib::emulator::Emulator::new();
-  let (p1_key, p2_key) =
-    controller::key_binding_parser::parse_key_binding("assets/keybindings.ini");
+  let (p1_key, p2_key) = controller::key_binding_parser::parse_key_binding(&args.key_binding_path);
   emulator.set_keys(p1_key, p2_key);
-  emulator.run("assets/test.nes");
+  emulator.run(&args.rom_path);
 }
